d8452a1cfe91669f3a61ab2bad52848424ca3797
toomadev <davisilvakid@gmail.com>
2026-05-14 11:00:45 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const { initDB }    = require('./database');
+const { startServer } = require('./server');
+const { startBot }    = require('./index');
+
+async function main() {
+    console.log('[MAIN] Iniciando banco de dados...');
+    await initDB();
+
+    console.log('[MAIN] Iniciando servidor Mini App...');
+    await startServer();
+
+    console.log('[MAIN] Iniciando bot Telegram...');
+    await startBot();
+
+    console.log('[MAIN] ✅ Tudo rodando!');
+}
+
+main().catch(err => {
+    console.error('[MAIN] Erro fatal:', err);
+    process.exit(1);
+});
